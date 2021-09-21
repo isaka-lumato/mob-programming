ac31441d1932c23af493f935c80a40cf1e54016a
isaka-lumato <75973193+isaka-lumato@users.noreply.github.com>
2021-09-21 10:12:00 +0300

    mob-programming

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,18 @@
 module.exports = function () {
-  // write your code here
+  let n = 100;
+let result = [];
+
+for (let i = 1; i <= n; i++) {
+if (i % 5 === 0 && i % 3 === 0) {
+result.push('FizzBuzz');
+} else if (i % 3 === 0) {
+result.push('Fizz');
+} else if (i % 5 === 0) {
+result.push('Buzz');
+} else {
+result.push(i);
+}
+}
+
+return result; 
 }
